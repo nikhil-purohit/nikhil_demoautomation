a2ce588dc12c9f92f593234a7d58761c15b62419
nikhilpurohit <nikhilpurohitbsw@gmail.com>
2021-06-01 02:06:02 +0530

    first commit

---
+++ src/test/java/tp_test/demo_test.java
@@ -0,0 +1,19 @@
+package tp_test;
+
+import  org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class demo_test {
+	
+	@Test
+	public void add()
+	{
+		System.out.println("sum");
+		int a=1;
+		int b = 3 ; 
+	System.out.println(a+b);
+	 Assert.assertEquals(4,a+b);
+				
+	}
+
+}
